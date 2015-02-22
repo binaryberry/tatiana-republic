8bdcb4c9b9931eadf190ac515e7b7aa2a2b25866
Tatiana Soukiassian <binaryberry@gmail.com>
2015-02-22 16:07:15 +0000

    wrote adding a product test, not passing yet

--- spec/CheckoutSpec.js
+++ spec/CheckoutSpec.js
@@ -1,16 +1,27 @@
 describe("Checkout", function() {
 	var checkout;
-	
+	var product;
+
 	beforeEach(function(){
 
 		checkout = new Checkout;
+		var product = new Product("Almond Toe Court Shoes, Patent Black", "Women’s Footwear", 99.0, 5);
 
 	});
 
-	describe("name", function(){
+	describe("empty basket", function(){
 
 		it("should have an empty basket when created", function(){
 			expect(checkout.basket.length).toEqual(0)
+		});
+
+	});
+
+	describe("adding a product to the basket", function(){
+
+		it("should allow the user to add a product to the basket", function(){
+			checkout.addProduct()
+			expect(checkout.basket[]).toEqual(0)
 		});
 
 	});
